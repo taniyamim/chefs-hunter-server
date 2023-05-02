a794838351ef641dfddb047329121cfdec6511cd
taniyamim <jannatulhafsa500@gmail.com>
2023-05-03 02:32:58 +0600

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
+
+
+const chefs = require('./data/chefs.json')
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('chefs server running')
+})
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
 })
 
 app.listen(port, () => {
