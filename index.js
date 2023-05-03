54aa389e2f714733e8dab790aeb4dfe327e1cbc3
taniyamim <jannatulhafsa500@gmail.com>
2023-05-03 20:45:01 +0600

    is wise added data added

--- index.js
+++ index.js
@@ -4,14 +4,30 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 
 
-const chefs = require('./data/chefs.json')
+const chefs = require('./data/chefs.json');
+const chefsRecipe = require('./data/chefsRecipe.json');
+
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('chefs server running')
 })
+
 app.get('/chefs', (req, res) => {
   res.send(chefs)
+})
+
+// app.get('/chefsRecipe', (req, res) => {
+//   res.send(chefsRecipe)
+// })
+
+app.get('/chefs/:id' , (req , res) => {
+    const id = (req.params.id) ;
+    console.log(id);
+    console.log(chefs);
+    const selectedChef = chefs.find(n => n.id == id)
+    res.send(selectedChef);
+    
 })
 
 app.listen(port, () => {
